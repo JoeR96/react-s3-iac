efdb73936dd2ac809b3dcf32c910610060842c1c
Joe Richardson <joeyrichardson96@gmail.com>
2024-04-25 15:14:38 +0100

    tf

--- acm.tf
+++ acm.tf
@@ -7,6 +7,18 @@ resource "aws_acm_certificate" "ssl_cert" {
   }
 }
 
+resource "aws_route53_record" "www_record" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "www.${var.domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.react_app_cf.domain_name
+    zone_id                = aws_cloudfront_distribution.react_app_cf.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
+
 resource "aws_route53_record" "cert_validation" {
   for_each = {
     for dvo in aws_acm_certificate.ssl_cert.domain_validation_options : dvo.domain_name => {
