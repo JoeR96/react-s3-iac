c7ed7bd757e3243ab37e814adf4db89f9279c3ad
Joe Richardson <joeyrichardson96@gmail.com>
2024-04-25 15:37:05 +0100

    tf

--- s3.tf
+++ s3.tf
@@ -7,16 +7,20 @@ resource "aws_s3_bucket" "react_app" {
   }
 
   force_destroy = true
+}
+
+resource "aws_s3_bucket_policy" "react_app_policy" {
+  bucket = aws_s3_bucket.react_app.id
 
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
-        Action    = "s3:GetObject",
+        Action    = ["s3:GetObject"],
         Effect    = "Allow",
         Principal = "*",
         Resource  = "${aws_s3_bucket.react_app.arn}/*"
-      },
+      }
     ]
   })
 }
