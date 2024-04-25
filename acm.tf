9fc6a3bf3a033164803f3520785d4c87a29e34ec
Joe Richardson <joeyrichardson96@gmail.com>
2024-04-25 15:19:02 +0100

    tf

--- acm.tf
+++ acm.tf
@@ -7,19 +7,7 @@ resource "aws_acm_certificate" "ssl_cert" {
   }
 }
 
-resource "aws_route53_record" "www_record" {
-  zone_id = aws_route53_zone.primary.zone_id
-  name    = "www.${var.domain_name}"
-  type    = "A"
-
-  alias {
-    name                   = aws_cloudfront_distribution.react_app_cf.domain_name
-    zone_id                = aws_cloudfront_distribution.react_app_cf.hosted_zone_id
-    evaluate_target_health = true
-  }
-}
-
-resource "aws_route53_record" "cert_validation" {
+resource "aws_route53_record" "cert_validation_record" {
   for_each = {
     for dvo in aws_acm_certificate.ssl_cert.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
@@ -28,7 +16,7 @@ resource "aws_route53_record" "cert_validation" {
     }
   }
 
-  zone_id = data.aws_route53_zone.primary.zone_id
+  zone_id = aws_route53_zone.primary.zone_id
   name    = each.value.name
   type    = each.value.type
   records = [each.value.record]
@@ -37,5 +25,5 @@ resource "aws_route53_record" "cert_validation" {
 
 resource "aws_acm_certificate_validation" "cert_validation" {
   certificate_arn         = aws_acm_certificate.ssl_cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation_record : record.fqdn]
 }
--- route53.tf
+++ route53.tf
@@ -1,9 +1,9 @@
-data "aws_route53_zone" "primary" {
-  name = var.domain_name 
+resource "aws_route53_zone" "primary" {
+  name = var.domain_name
 }
 
 resource "aws_route53_record" "www_record" {
-  zone_id = data.aws_route53_zone.primary.zone_id
+  zone_id = aws_route53_zone.primary.zone_id
   name    = "www.${var.domain_name}"
   type    = "A"
 
