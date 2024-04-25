2e074f99bbbd9db6d5a9af62e5f4bd8117f6d8b5
Joe Richardson <joeyrichardson96@gmail.com>
2024-04-25 15:35:30 +0100

    tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,5 @@
 resource "aws_s3_bucket" "react_app" {
-  bucket = var.bucket_name
-  acl    = "public-read"
+  bucket = "${var.domain_name}-${var.env}"
 
   website {
     index_document = "index.html"
@@ -8,20 +7,16 @@ resource "aws_s3_bucket" "react_app" {
   }
 
   force_destroy = true
-}
-
-resource "aws_s3_bucket_policy" "react_app_policy" {
-  bucket = aws_s3_bucket.react_app.id
 
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
-        Action    = ["s3:GetObject"],
+        Action    = "s3:GetObject",
         Effect    = "Allow",
         Principal = "*",
         Resource  = "${aws_s3_bucket.react_app.arn}/*"
-      }
+      },
     ]
   })
 }
