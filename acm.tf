1789d9fb6b4c73636fc9fea57218ef1d13f3aa75
Joe Richardson <joeyrichardson96@gmail.com>
2024-04-25 16:26:06 +0100

    tftf

--- acm.tf
+++ acm.tf
@@ -1,10 +1,11 @@
 provider "aws" {
-    region = "us-east-1" 
+    alias = "us-east-1"
 }
 
 resource "aws_acm_certificate" "ssl_cert" {
   domain_name       = var.domain_name
   validation_method = "DNS"
+  provider            = aws.us-east-1
 
   lifecycle {
     create_before_destroy = true
--- versions.tf
+++ versions.tf
@@ -8,7 +8,7 @@ terraform {
     }
   }
 }
-
 provider "aws" {
-  region = "eu-west-2"
+    alias  = "us-east-1"
+    region = "us-east-1"
 }
