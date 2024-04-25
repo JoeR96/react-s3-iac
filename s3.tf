b2596f2adace29bf85c93258102703b4765e39ea
Joe Richardson <joeyrichardson96@gmail.com>
2024-04-26 00:35:23 +0100

    tf

--- s3.tf
+++ s3.tf
@@ -7,4 +7,8 @@ resource "aws_s3_bucket" "react_app" {
   }
 
   force_destroy = true
+
+  # Enable public read access for all objects in the bucket
+  # This sets the bucket policy to allow public read access
+  acl = "public-read"
 }
